16a784534d7f3606475485e02a5d183b7b295501
Mukta Joshi <71441306+josh3397@users.noreply.github.com>
2025-05-15 13:27:14 +0300

    Update fastp.sh

--- fastp.sh
+++ fastp.sh
@@ -6,9 +6,16 @@
 #SBATCH --ntasks=1
 #SBATCH --mem-per-cpu=100G
 
-for file1 in *_R1.fastq;
+module load biokit
+
+inutdir="./Raw_data/"
+outdir="./Raw_trimmed"
+
+mkdir -p $outdir
+
+for file1 in $inputdir/*_1.fastq.gz;
 do
-file2=${file1/R1/R2};
-out=${file1%%.fastq}_output;
-/path_to_/fastp -i $file1 -I $file2 -o ${file1}_trimmed -O ${file2}_trimmed --length_required 100 --low_complexity_filter --detect_adapter_for_pe;
+file2=${file1/_1/_2};
+Sample=$(basename "$file1" | sed 's/_1.*//')
+fastp -i $file1 -I $file2 -o ${Sample}_1_trimmed.fastq.gz -O ${Sample}_2_trimmed.fastq.gz --length_required 100 -y -g;
 done
